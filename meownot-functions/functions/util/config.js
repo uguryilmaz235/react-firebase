f33989231316f1dd54e932e5ebd25ed09d35e1cf
Ugur YILMAZ <uguryilmaz235@gmail.com>
2019-08-31 22:03:22 +0300

    Part 7 ended and no bug

---
+++ meownot-functions/functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyAda_WiJtrH4bng6vc2vSwGgWnzxpyu2oE",
+  authDomain: "meow-not.firebaseapp.com",
+  databaseURL: "https://meow-not.firebaseio.com",
+  projectId: "meow-not",
+  storageBucket: "meow-not.appspot.com",
+  messagingSenderId: "962613483826",
+  appId: "1:962613483826:web:223cb03c744aa62d"
+};
