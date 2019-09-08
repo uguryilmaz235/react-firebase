f72eacec1c2b1485bf5bef38a8212e4caedab5fc
Ugur YILMAZ <uguryilmaz235@gmail.com>
2019-09-08 13:43:55 +0300

    part 11 finished, like unlike delete scream

--- meownot-functions/functions/index.js
+++ meownot-functions/functions/index.js
@@ -8,7 +8,10 @@ const {
   getAllScreams,
   postOneScream,
   getScream,
-  commentOnScream
+  commentOnScream,
+  likeScream,
+  unlikeScream,
+  deleteScream
 } = require("./handlers/screams");
 const {
   signup,
@@ -22,9 +25,9 @@ const {
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
 app.get("/scream/:screamId", getScream);
-// TODO delete scream
-// TODO like scream
-// TODO unlike scream
+app.delete("/scream/:screamId", FBAuth, deleteScream);
+app.get("/scream/:screamId/like", FBAuth, likeScream);
+app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
 app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
 
 // users routes
