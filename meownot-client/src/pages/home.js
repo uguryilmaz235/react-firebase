d5694d21c170d5b6cf3d35c1db7073e0cb3fe7f1
Ugur YILMAZ <uguryilmaz235@gmail.com>
2019-09-16 16:59:20 +0300

    Part 16 Login Form was completed

--- meownot-client/src/pages/home.js
+++ meownot-client/src/pages/home.js
@@ -11,7 +11,6 @@ class home extends Component {
     axios
       .get("/screams")
       .then(res => {
-        console.log(res.data);
         this.setState({
           screams: res.data
         });
@@ -20,7 +19,9 @@ class home extends Component {
   }
   render() {
     let recentScreamsMarkup = this.state.screams ? (
-      this.state.screams.map(scream => <Scream scream={scream} />)
+      this.state.screams.map(scream => (
+        <Scream key={scream.screamId} scream={scream} />
+      ))
     ) : (
       <p>Loading...</p>
     );
