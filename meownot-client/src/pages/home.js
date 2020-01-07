0f3a994a7201ef6d34a984a30dafc0a59803a150
Ugur YILMAZ <uguryilmaz235@gmail.com>
2020-01-08 00:36:20 +0300

    end of  part 21 profile upload

--- meownot-client/src/pages/home.js
+++ meownot-client/src/pages/home.js
@@ -27,7 +27,7 @@ class home extends Component {
       <p>Loading...</p>
     );
     return (
-      <Grid container spacing={10}>
+      <Grid container spacing={16}>
         <Grid item sm={8} xs={12}>
           {recentScreamsMarkup}
         </Grid>
