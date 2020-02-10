ddac5e69ade3bfaf050f83288ac894a527e893bf
Ugur YILMAZ <uguryilmaz235@gmail.com>
2020-02-10 20:36:51 +0300

    like and unlike scream

--- meownot-client/src/redux/reducers/userReducer.js
+++ meownot-client/src/redux/reducers/userReducer.js
@@ -1,11 +1,10 @@
 import {
   SET_USER,
-  //SET_ERRORS,
-  //CLEAR_ERRORS,
-  //LOADING_UI,
   SET_AUTHENTICATED,
   SET_UNAUTHENTICATED,
-  LOADING_USER
+  LOADING_USER,
+  LIKE_SCREAM,
+  UNLIKE_SCREAM
 } from "../types";
 
 const initialState = {
@@ -35,6 +34,24 @@ export default function(state = initialState, action) {
       return {
         ...state,
         loading: true
+      };
+    case LIKE_SCREAM:
+      return {
+        ...state,
+        likes: [
+          ...state.likes,
+          {
+            userHandle: state.credentials.handle,
+            screamId: action.payload.screamId
+          }
+        ]
+      };
+    case UNLIKE_SCREAM:
+      return {
+        ...state,
+        likes: state.likes.filter(
+          like => like.screamId !== action.payload.screamId
+        )
       };
     default:
       return state;
